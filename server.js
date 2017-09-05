5cf1dbf112053a14d1b3dfa8b491dcc8add06399
kamatp <31243045+kamatp@users.noreply.github.com>
2017-09-05 22:33:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var artilceOne = {
+var articleOne = {
     title: 'Article One | Pallavi Kamat',
     heading: 'Article One',
     date: 'Sep 5, 2017',
