e80708739df6887dc3c3ba8d67d2c1fc74d4a36d
kamatp <31243045+kamatp@users.noreply.github.com>
2017-09-05 22:31:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.use(morgan('combined'));
 
 var content = {
     title: 'Article One | Pallavi Kamat',
+    heading: 'Article One',
     date: 'Sep 5, 2017',
     content: `
     <p>
@@ -20,12 +21,49 @@ var content = {
     </p>`
 };
 
+function createTemplate (data) {
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    
+    var htmlTemplate = `
+    <html>
+        <head>
+            <title>
+                ${title}
+            </title>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link href="/ui/style.css" rel="stylesheet" />
+    
+        </head>
+        <body>
+            <div class="container">
+                <div>
+                    <a href="/">Home</a>
+                </div>
+                <hr/>
+                <h3>
+                    ${heading}
+                </h3>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                    ${content}
+                </div>
+            </div>
+        </body>
+    </html>
+    `;
+    return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function (req, res) {
