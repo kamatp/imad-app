0f250197d7d380097048a3030aef365a7891bac2
kamatp <31243045+kamatp@users.noreply.github.com>
2017-09-06 14:46:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,9 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML='new One';
+
+//Move the image
+document.getElementById('img');
+img.onclick = function () {
+    img.style.marginLeft = '100px';
+}
