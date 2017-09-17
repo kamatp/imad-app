f681a18066cf86dc2b70e96636a01d9586d51876
kamatp <31243045+kamatp@users.noreply.github.com>
2017-09-17 20:29:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,11 +17,27 @@ button.onClick = function () {
             }
         }
         // Not done yet
-    }
+    };
 
     //Make the request
     request.open('GET','http://kamatpallavi.imad.hasura-app.io/counter', true);
     request.send(NULL);
 
+//Submit name
 
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onClick = function() {
+    //Make a reques to the server and send the name
+    
+    //Capture a list of names and render it as a list
+    var names = ['name1', 'name2', 'name3', 'name4'];
+    var list = '';
+    for (var i=0; i < names.length; i++) {
+        list == '<li>' + names[i] + '</li>';
+    }
+    var vl = document.getElementById('namelist');
+    vl.innerHTML = list;
+};
 };
