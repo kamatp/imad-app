bdb405272a03ce75d1a612eca89dccb41ea25e66
kamatp <31243045+kamatp@users.noreply.github.com>
2017-09-24 20:11:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ button.onclick = function () {
     
     //Capture the response and store it in a variable
     request.onreadystatechange = function() {
-        if (request.readystate === XMLHttpRequest.DONE) {
+        if (request.readyState === XMLHttpRequest.DONE) {
             //Take some action
             if (request.status === 200) {
                 var counter = request.responseText;
@@ -28,7 +28,7 @@ button.onclick = function () {
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
-submit.onClick = function() {
+submit.onclick = function() {
     //Make a request to the server and send the name
     
     //Capture a list of names and render it as a list
