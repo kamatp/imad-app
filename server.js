e1f2b1db3b5265875392115b1699a53f16ffd582
kamatp <31243045+kamatp@users.noreply.github.com>
2017-09-25 18:05:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,6 +92,19 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/test-db', function (req, res) {
+    // make a select request
+    // return a response with the results
+    pool.query('SELECT * FROM test', function(err, result){
+        if (err) {
+            res.status(500).send(err.toString());
+        } else {
+            res.send(JSON.stringify(result))
+        }
+        }
+    })
+}
+
 var counter = 0;
 app.get('/counter', function (req, res) {
     counter = counter + 1;
