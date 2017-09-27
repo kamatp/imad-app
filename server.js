226bee6b8274248579386024d75037ae699e4d39
kamatp <31243045+kamatp@users.noreply.github.com>
2017-09-27 17:24:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.get('/', function (req, res) {
 function hash(input, salt){
     //create a hash
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return ["pbkdf2", "10000", salt, hashed.toString].join('$');
+    return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$');
 }
 
 app.get('/hash/:input', function (req, res) {
