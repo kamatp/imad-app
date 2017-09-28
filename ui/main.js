000e590dc5994af117bff36e66db7203d6de6b08
kamatp <31243045+kamatp@users.noreply.github.com>
2017-09-28 16:52:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,43 +1,33 @@
-//Counter code
-var button = document.getElementById('counter');
+//Submit username and password to login
 
-button.onclick = function () {
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
     
     // Create a request object
-    var request = new XMLHttpRequest();
+    var request = XMLHttpRequest();
     
     //Capture the response and store it in a variable
     request.onreadystatechange = function() {
-        if (request.readyState === XMLHttpRequest.DONE) {
+        if (request.readyState === XMTHttpRequest.DONE) {
             //Take some action
             if (request.status === 200) {
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
+                alert ("Logged in successfully");
+            } else if (request.status === 403) {
+                alert("Username/Password is incorrect");
+            } else if (request.status === 500) {
+                alert("Something went wrong on the server");
             }
         }
-        // Not done yet
+        //Not Done yet
     };
-
-    //Make the request
-    request.open('GET','http://kamatpallavi.imad.hasura-app.io/counter', true);
-    request.send(null);
-
-//Submit name
-
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
-var submit = document.getElementById('submit_btn');
-submit.onclick = function() {
-    //Make a request to the server and send the name
     
-    //Capture a list of names and render it as a list
-    var names = ['name1', 'name2', 'name3', 'name4'];
-    var list = '';
-    for (var i=0; i < names.length; i++) {
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
-};
+    //Make the request
+    var username =  document.getElementById('username').value;
+    var password =  document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST', 'http://kamatpallavi.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify( {username: username, password: password}));
+    
 };
